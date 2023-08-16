0f561faa78d34186754755c85aadb889421fde58
Tich123 <mashavatich@gmail.com>
2023-08-16 10:34:27 -0700

    python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    return [[x**2 for x in row] for row in matrix]
-
+    if(matrix):
+        return ([[y*y for y in x] for x in matrix])
